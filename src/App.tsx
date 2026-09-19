89e57e494810af0bfaad653754117e19db4426cd
dev-m-josh <mutambukijoshua2@gmail.com>
2026-09-20 00:22:24 +0300

    Intiated  my calc react, tailwind practice

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Calculator
+      </h1>
+    </main>
+  );
+}
+
+export default App;
